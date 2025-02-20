d942410daab978f56f72333ef045b35481c453bf
Kavin <129659804+Kavin-crew@users.noreply.github.com>
2025-02-20 13:32:01 +0800

    change product Id

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "1501319168099";
+  let ProductId = "1501318283363";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
